1366989b0a662b292bf306c7997103004af3afbb
chaaanito <christian.carl.donor@gmail.com>
2025-10-26 16:58:50 +0800

    update

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,25 @@
   import Router from 'svelte-spa-router'
   import { Toaster } from 'svelte-sonner'
   import wrap from 'svelte-spa-router/wrap'
+  import Player from './routes/Player.svelte'
+  import Guest from './routes/Guest.svelte'
+
+  // const routes = {
+  //   '/': wrap({
+  //     asyncComponent: () => import('./routes/Player.svelte'),
+  //   }),
+  //   '/:id': wrap({
+  //     asyncComponent: () => import('./routes/Guest.svelte'),
+  //   }),
+  //   '*': wrap({
+  //     asyncComponent: () => import('./routes/Player.svelte'),
+  //   }),
+  // }
 
   const routes = {
-    '/': wrap({
-      asyncComponent: () => import('./routes/Player.svelte'),
-    }),
-    '/:id': wrap({
-      asyncComponent: () => import('./routes/Guest.svelte'),
-    }),
-    '*': wrap({
-      asyncComponent: () => import('./routes/Player.svelte'),
-    }),
+    '/': Player,
+    '/:id': Guest,
+    '*': Player,
   }
 </script>
 
